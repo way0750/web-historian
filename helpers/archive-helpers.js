e657f2ff02cebca1e4cb10d3e6f820257d9559a3
way0750 <bruce0750@gmail.com>
2015-10-28 17:28:54 -0700

    Implemented helpers to check list of urls

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -25,11 +25,25 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function() {
-  
+exports.readListOfUrls = function(callBack) {
+  //get all urls in array as strings
+  // callBack(that array)
+
+  // get all urls: need to get path
+  var filePath = exports.paths.list
+  // read all content and split by \n
+  fs.readFile(filePath, 'utf-8', function (err, data) {
+    callBack(data.split('\n'));
+  })
+  // console.log('--------------------', urls);
+
 };
 
-exports.isUrlInList = function() {
+exports.isUrlInList = function(url, callback) {
+  var urlArr;
+  exports.readListOfUrls(function (urls) {
+    callback(urls.indexOf(url) > -1);
+  })
 };
 
 exports.addUrlToList = function() {
--- web/request-handler.js
+++ web/request-handler.js
@@ -5,24 +5,17 @@ var httpHelper = require('./http-helpers');
 
 exports.handleRequest = function (req, res) {
   // to send a list of all sites that are already cached on server
-  // res.end(archive.paths.list);
-  // if(req.url === '/' && req.method === 'GET') {
-  // }
-  // var pathName = req.url === '/' ? '/index.html' : req.url;
-  // /archive.paths.siteAssets, pathName
-  // console.log(pathName);
+  var pathName = req.url === '/' ? '/index.html' : req.url;
+  // archive.paths.siteAssets, pathName
   // if(req.method === 'GET' && req.url === '/'){
-    // res.end(__dirname)
-    httpHelper.serveAssets(res, '/public/index.html', function(data) {
-      // if(err) {
-      //   res.writeHead(404, httpHelper.headers);
-      //   res.end();
-      // } else {
+    httpHelper.serveAssets(res, path.join(archive.paths.siteAssets, pathName), function(err, data) {
+      if(err) {
+        res.writeHead(404, httpHelper.headers);
+        res.end()
+      } else {
         res.writeHead(200, httpHelper.headers);
-      //   console.log('--------------', data);
         res.end(data);
-
-      // }
+      }
     })
   // } else if() {
   //   res.writeHead(404, httpHelper.headers);
@@ -34,15 +27,4 @@ exports.handleRequest = function (req, res) {
 // have routing mechinism, 
 // if the route matches with one of the caches sites then serve it
 // if not then redirect user to loading page, start finding site and adding it into archive text file
-
-
-
-        // var body;
-        // res.on('data', function(chunk) {
-        //   body += chunk;
-        // });
-        // res.on('end', function() {
-        //   res.writeHead(200, httpHelper.headers);
-        //   console.log('--------------', data);
-        //   res.end(body);
-        // });
+// http://localHost:8080/www.google.com
