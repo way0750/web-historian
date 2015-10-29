8f584ec50241b2ed0d119dab8eee239a426ed311
way0750 <bruce0750@gmail.com>
2015-10-29 11:46:15 -0700

    Refactored response into serve assets in order to clean up and remove repetitive code

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var httpHelper = require('./http-helpers');
+var urlCheck = archive.isUrlInList;
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
@@ -8,36 +9,27 @@ exports.handleRequest = function (req, res) {
   var pathName = req.url === '/' ? '/index.html' : req.url;
   // archive.paths.siteAssets, pathName
   // if(req.method === 'GET' && req.url === '/'){
-  httpHelper.serveAssets(res, path.join(archive.paths.siteAssets, pathName), function(err, data) {
-    if(err) {
-      res.writeHead(404, httpHelper.headers);
-      res.end()
+  if(req.method === 'GET') {
+    if(req.url === '/') {
+      httpHelper.serveAssets(res, path.join(archive.paths.siteAssets, pathName))
     } else {
-      res.writeHead(200, httpHelper.headers);
-      res.end(data);
+      httpHelper.serveAssets(res, path.join(archive.paths.archivedSites, pathName))
     }
-  })
+  }
   if(req.method === 'POST') {
     var formData = "";
     req.on('data', function (data) {
       formData += data;
-      // console.log('-------------->', "a chuck of data: ", data);
     })
-    // console.log('---------------> data is:', formData);
     req.on('end', function (err, data) {
       formData = formData.split('=')[1];
-      archive.addUrlToList(formData);
-      res.writeHead(302, httpHelper.headers);
-      res.end()
-    })
-    
-
-
+      archive.addUrlToList(formData, function() {
+        res.writeHead(302, httpHelper.headers);
+        res.end()
+      }, res);
+    });
   }
-  // } else if() {
-  //   res.writeHead(404, httpHelper.headers);
-  //   res.end();
-  // }
+
 };
 
 
